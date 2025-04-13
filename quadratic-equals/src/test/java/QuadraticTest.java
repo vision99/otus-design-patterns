e3c947ab63e2a2abd3cb275aa341e58a25238564
Dobranov Alexei <alex_d_80@mail.ru>
2025-04-13 17:46:22 +0300

    добавлены тэги к тестам

--- quadratic-equals/src/test/java/QuadraticTest.java
+++ quadratic-equals/src/test/java/QuadraticTest.java
@@ -1,9 +1,10 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.otus.alex.QuadraticEqualsCalculatorImpl;
-
+@Tag( "QuadraticEquals" )
 public class QuadraticTest {
 
     /*
