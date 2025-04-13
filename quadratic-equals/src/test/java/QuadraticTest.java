56e0e32dc0ab4ba98f412220f2cb91443bb9b8a9
Dobranov Alexei <alex_d_80@mail.ru>
2025-04-13 18:16:10 +0300

    добавлен ссылка на переменную для билда vars.MODULE_NAME_QUADRATIC_EQUALS.

--- quadratic-equals/src/test/java/QuadraticTest.java
+++ quadratic-equals/src/test/java/QuadraticTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.otus.alex.QuadraticEqualsCalculatorImpl;
-@Tag( "QuadraticEquals" )
+@Tag( "quadratic-equals" )
 public class QuadraticTest {
 
     /*
